f9460cfe3d3feeb0bc5162cc0042844a17c43c31
Gonzalo Casanova <gonicasanova.gc@gmail.com>
2022-08-20 01:02:47 -0300

    added employeeRouter, errorHandler and NotFound response

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv"
 dotenv.config()
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response} from "express"
 import morgan from "morgan"
 import cors from "cors"
 require("./database")
+import employeeRouter from "../routes/employee.router"
+import { EmployeeError } from "../errors/employee.error"
 
 const app = express()
 
@@ -17,6 +19,19 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"]
 }))
+
+app.use("/employee", employeeRouter)
+
+// Error handler, si el error es un EmployeeError devuelve el mensaje con su código, sino el mensajo con código error 500
+app.use((error: EmployeeError, _req: Request, res: Response, _next: NextFunction) => {
+  return error.code ?
+    res.status(error.code).json({ message: error.message }) :
+    res.status(500).json({ message: error.message })
+})
+
+app.use((_req: Request, res: Response) => {
+  return res.status(404).json({ message: "Page not found" })
+})
 
 app.listen(app.get("port"), () => {
   console.log(`App listening on port ${app.get("port")}`)
