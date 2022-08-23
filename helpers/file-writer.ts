95032820f874bfbcdcf9ea87300e260e1dbeec25
Gonzalo Casanova <gonicasanova.gc@gmail.com>
2022-08-23 01:43:29 -0300

    improvements in writeFile function

--- helpers/file-writer.ts
+++ helpers/file-writer.ts
@@ -3,34 +3,40 @@ const convertCsvToXlsx = require("@aternus/csv-to-xlsx")
 import fs from "fs"
 import path from "path"
 
-export const writeFile = (employees: Array<Employee>) => {
+// Función que escribe un archivo CSV, lo transforma en el excel a exportar y devuelve el path donde se guarda
+export const writeFile = (employee: Employee, dependents: Array<Employee>): string => {
 
   let content: string = writeHeader()
 
+  const employees = [employee, ...dependents]
+
+  // Por cada empleado se escribe una linea del archivo CSV
   employees.forEach(employee => {
     content += writeLine(employee)
   })
 
-  const csvPath: string = path.join(__dirname, `../temp/Info_${employees[0].apellido}_${employees[0].nombre}.csv`)
-
+  const csvPath: string = path.join(__dirname, `../temp/Info_${employee.apellido}_${employee.nombre}.csv`)
   fs.writeFileSync(csvPath, content)
+  const xlsxPath = path.join(__dirname, `../temp/Info_${employee.apellido}_${employee.nombre}.xlsx`)
 
   try {
-    const xlsxPath = path.join(__dirname, `../temp/Info_${employees[0].apellido}_${employees[0].nombre}.xlsx`)
     convertCsvToXlsx(csvPath, xlsxPath)
+    removeFile(csvPath)
   } catch(error: any) {
     throw error
   }
 
-  removeFile(csvPath)
+  return xlsxPath
 }
 
+// Header del archivo a exportar
 const writeHeader = (): string => {
   return "Apellido y Nombre,Legajo,DNI,Edad,Rol,Superior Inmediato,Gerencia,Sector\n"
 }
 
+// Linea CSV que contiene la información del empleado
 const writeLine = (employee: Employee): string => {
-  return `${employee.apellido} ${employee.nombre},${employee.legajo},${employee.dni},${employee.getAge(employee)},${employee.rol},${employee.dni_jefe},${employee.gerencia},${employee.sector}\n`
+  return `${employee.apellido} ${employee.nombre},${employee.legajo},${employee.dni},${employee.getAge()},${employee.rol},${employee.dni_jefe ? employee.dni_jefe : ""},${employee.gerencia},${employee.sector}\n`
 }
 
 export const removeFile = (path: string) => {
