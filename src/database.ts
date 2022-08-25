2209f0581356618ae4b744975d64354f6379cefd
Gonzalo Casanova <gonicasanova.gc@gmail.com>
2022-08-25 02:01:49 -0300

    exporting database connect in async function

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,10 @@
 import { config } from "../config"
 import mongoose from "mongoose";
 
-mongoose.connect(config.DB_URI)
-  .then(() => console.log("Mongo client connected"))
-  .catch(error => console.error(error))
+export const connectDatabase = async () => {
+  await mongoose.connect(config.DB_URI)
+    .then(() => console.log("Mongo client connected"))
+    .catch(error => console.error(error))
+}
 
-module.exports = mongoose
+// module.exports = mongoose
